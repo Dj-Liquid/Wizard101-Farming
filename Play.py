ecaa719f1671c17b29e3b1e7b15a8e1ab891bab1
Dj-Liquid <114222687+Dj-Liquid@users.noreply.github.com>
2024-12-14 00:01:27 -0500

    Semi Auto-Play functionality

--- Play.py
+++ Play.py
@@ -13,12 +13,38 @@ yellow = (0, 255, 255)
 green = (0, 255, 0)
 pause_State = True
 stop_threads = False
+w = gw.getWindowsWithTitle("Wizard101")[0]
 #WizardGraphicalClient
 #Wizard101
 
 def Choose_Spell(position):
-    mouse.click(left)
-    
+    #current_position = mouse.get_position()
+    print("One Card",position)
+    #print("Mouse",current_position[0])
+    #mouse.drag(current_position[0], current_position[1], position[0], position[1], absolute=True, duration=0.2)
+    mouse.move(position[0], position[1], absolute=True, duration=0.15)
+    time.sleep(.2)
+    mouse.click('left')
+    time.sleep(.2)
+
+def Attack():
+    location = 0
+    attack = cv2.imread('Epic_Zand.png')
+    w.activate()
+    img = pyautogui.screenshot(region=(w.left, w.top, w.width, w.height))
+    width, height = attack.shape[:-1]
+    menu = cv2.cvtColor(np.array(img), cv2.COLOR_RGB2BGR)
+    #menu = cv2.imread('Test.png')
+    match = cv2.matchTemplate(menu, attack, cv2.TM_CCOEFF_NORMED)
+    loc = np.where(match >= threshold)
+    for point in zip(*loc[::-1]):  # Switch columns and rows
+        cv2.rectangle(menu, point, (point[0] + height, point[1] + width), (255, 255, 255), 2)
+        location = [point[0] + width // 2, point[1] + height // 2]
+    cv2.imwrite('result2.png', menu)
+    if location != 0:
+        Choose_Spell(location)
+    else:
+        Attack()
     
 def Enchant(epic_position,zand_position):
     Choose_Spell(epic_position)
@@ -26,12 +52,13 @@ def Enchant(epic_position,zand_position):
 
 def Play_Round():
     positions = Get_Cards()
-    print(positions)
-    #Enchant(positions[0],positions[1])
+    #print("Both cards",positions)
+    Enchant(positions[0],positions[1])
+    mouse.move(0, 400, absolute=False, duration=0.15)
+    time.sleep(5)
+    Attack()
 
 def Get_Cards():
-    w = gw.getWindowsWithTitle("Wizard101")[0]
-    # activate the window
     w.activate()
     img = pyautogui.screenshot(region=(w.left, w.top, w.width, w.height))
     menu = cv2.cvtColor(np.array(img), cv2.COLOR_RGB2BGR)
@@ -47,11 +74,11 @@ def Get_Cards():
         location = np.where(match >= threshold)
         for point in zip(*location[::-1]):  # Switch columns and rows
             cv2.rectangle(menu, point, (point[0] + card_width, point[1] + card_height), card[1], 2)
-            locations.append([point, (point[0] + card_width, point[1] + card_height)])
+            locations.append([point[0] + card_width // 2, point[1] + card_height // 2])
     
     cv2.imwrite('result.png', menu)
     
-    return locations
+    return locations[:2]
 '''
 def Test():
     global pause_State
